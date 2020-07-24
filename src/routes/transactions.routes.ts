2b1dcbd8680838bd202e75a1cbcb38f22e7d994c
Camila Sales <salescamila1@gmail.com>
2020-07-24 16:45:45 -0400

    feat: Listar transações

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,13 +2,18 @@ import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import { getCustomRepository } from 'typeorm';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionRepository.find();
+
+  return response.json(transactions);
 });
 
 transactionsRouter.post('/', async (request, response) => {
